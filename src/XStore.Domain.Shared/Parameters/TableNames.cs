ec256120eecf024e6bec99fa663bf04e47d16a0c
brunoverc <69854207+brunoverc@users.noreply.github.com>
2023-03-08 16:55:02 -0300

    Begin XStore.Infra.Data

---
+++ src/XStore.Domain.Shared/Parameters/TableNames.cs
@@ -0,0 +1,12 @@
+﻿namespace XStore.Domain.Shared.Parameters
+{
+    public class TableNames
+    {
+        public static string TbAddress => "xs_Address";
+        public static string TbClient => "xs_Client";
+        public static string TbOrder => "xs_Order";
+        public static string TbOrderItem => "xs_OrderItem";
+        public static string TbProduct => "xs_Product";
+        public static string TbVoucher => "xs_Voucher";
+    }
+}
---
+++ src/XStore.Infra.Data/Mapping/AddressMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XStore.Domain.Entities;
+
+namespace XStore.Infra.Data.Mapping
+{
+    public class AddressMap : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+
+        }
+    }
+}
