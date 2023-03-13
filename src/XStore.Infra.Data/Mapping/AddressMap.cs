3e9290b7e0f55a33d9b9301ea32960121f938d1f
brunoverc <69854207+brunoverc@users.noreply.github.com>
2023-03-13 15:18:23 -0300

    Add AddressMap

--- src/XStore.Infra.Data/Mapping/AddressMap.cs
+++ src/XStore.Infra.Data/Mapping/AddressMap.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using XStore.Domain.Entities;
+using XStore.Domain.Shared.Parameters;
 
 namespace XStore.Infra.Data.Mapping
 {
@@ -13,7 +9,14 @@ namespace XStore.Infra.Data.Mapping
     {
         public void Configure(EntityTypeBuilder<Address> builder)
         {
-
+            builder.ToTable(TableNames.TbAddress); //Define o nome da tabela
+            builder.Property(a => a.Street).HasMaxLength(200).IsRequired();//Tamanho máximo de 200 e obrigatória
+            builder.Property(a => a.Number).HasMaxLength(5);
+            builder.Property(a => a.Complement).HasMaxLength(200);
+            builder.Property(a => a.Neighborhood).HasMaxLength(100).IsRequired();
+            builder.Property(a => a.City).HasMaxLength(50).IsRequired();
+            builder.Property(a => a.State).HasMaxLength(2).IsRequired();
+            builder.Ignore(a => a.Deleted);
         }
     }
 }
---
+++ src/XStore.Infra.Data/Mapping/ClientMap.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XStore.Domain.Entities;
+using XStore.Domain.Shared.Parameters;
+using XStore.Core.DomainObjects;
+
+namespace XStore.Infra.Data.Mapping
+{
+    public class ClientMap : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.ToTable(TableNames.TbClient); //Define o nome da tabela
+            //Name => Tamanho máximo de 100, Obrigatório
+            //Email => Tamanho máximo de 100, Obrigatório
+            //Cpf => Tamanho máximo de 14, Obrigatório
+            
+            
+            //Active
+            //AddressId
+
+            builder.Ignore(a => a.Deleted);
+        }
+    }
+}
