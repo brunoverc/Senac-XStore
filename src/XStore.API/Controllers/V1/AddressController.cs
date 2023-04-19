8bb16f5232911038b5ed6c8a0cbe18de95d53876
brunoverc <69854207+brunoverc@users.noreply.github.com>
2023-04-19 17:38:56 -0300

    First API

---
+++ src/XStore.API/Controllers/V1/AddressController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XStore.Application.Interfaces;
+using XStore.Application.ViewModel;
+
+namespace XStore.API.Controllers.V1
+{
+    [ApiController]
+    [Route("api/v1/address")]
+    public class AddressController : Controller
+    {
+        private readonly IAddressAppService _addressAppService;
+
+        public AddressController(IAddressAppService addressAppService)
+        {
+            _addressAppService = addressAppService;
+        }
+
+        [HttpGet]
+        // Traz todos os endereços cadastrados no sistema
+        public async Task<ActionResult> Get()
+        {
+            var result = await _addressAppService.SearchAsync(a => true);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        // Traz um Endereço de acordo com o seu ID
+        public async Task<ActionResult<AddressViewModel>> Get(Guid id)
+        {
+            var result = await _addressAppService.GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        //Insere um endereço no banco de dados
+        public async Task<ActionResult> PostAsync([FromBody] AddressViewModel model)
+        {
+            var result = await _addressAppService.AddAsync(model);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(Guid id, [FromBody] AddressViewModel model)
+        {
+            var address = await _addressAppService.GetByIdAsync(id);
+            if(address == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_addressAppService.Update(model));
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            _addressAppService.Remove(id);
+            return Ok();
+        }
+    }
+}
---
+++ src/XStore.Core/Model/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace XStore.Core.Model
+{
+    public class Response
+    {
+        public bool Success { get; set; }
+        public object Data { get; set; }
+        public ICollection<string> Message { get; set; } = new List<string>();
+    }
+}
